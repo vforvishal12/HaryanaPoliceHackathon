0c550bcbce9e9c6789aea178ba4656965e7491b9
Vishal Kumar Saw <vishal.saw@hm.com>
2023-12-24 18:23:46 +0530

    y

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,36 +1,7 @@
 const projectsData = [
   {
     title: "Our Drug Culture",
-    description: `India, like many other countries, has a complex relationship with drugs, encompassing both traditional and contemporary aspects. Here are some key points related to drug culture in India:
-
-    Traditional Drug Use:
-    
-    Ayurveda and Traditional Medicine: India has a rich history of traditional medicine, including Ayurveda, which utilizes various herbs and plants for medicinal purposes. Some of these substances may have psychoactive properties, and their use is often deeply ingrained in cultural and religious practices.
-    Cannabis Use:
-    
-    Cultural and Religious Significance: Cannabis has been used in religious rituals in India for centuries. It is associated with Lord Shiva and is commonly consumed during festivals like Holi and Mahashivratri.
-    Legal Status: The legal status of cannabis is a topic of debate in India. While the recreational use of cannabis is largely illegal, there are some states where the use of marijuana for medicinal and religious purposes is permitted.
-    Opium and Other Narcotics:
-    
-    Opium Cultivation: India has a history of opium cultivation, and it remains one of the largest legal producers of opium for pharmaceutical purposes. However, illegal cultivation and trafficking of opium and its derivatives also pose significant challenges.
-    Illicit Drug Trade:
-    
-    Narcotics Trafficking: India faces challenges related to the trafficking of illicit drugs, including heroin, cocaine, and synthetic drugs. The country is both a source and a transit point for the illegal drug trade.
-    Government Initiatives: The government has implemented various measures to combat drug trafficking, including the enforcement of the Narcotic Drugs and Psychotropic Substances Act.
-    Youth and Substance Abuse:
-    
-    Increasing Concerns: There are growing concerns about substance abuse, especially among the youth, in urban areas. Commonly abused substances include alcohol, tobacco, prescription drugs, and various synthetic drugs.
-    Government Response:
-    
-    Legislation and Enforcement: The Indian government has enacted legislation to regulate and control the use of narcotics and psychotropic substances. However, the enforcement of these laws faces challenges due to factors such as corruption, poverty, and limited resources.
-    Public Awareness and Rehabilitation:
-    
-    Awareness Programs: Various organizations and government agencies work on awareness programs to educate the public about the dangers of drug abuse.
-    Rehabilitation Centers: There are rehabilitation centers across the country that provide support and treatment for individuals struggling with substance abuse.
-    International Cooperation:
-    
-    Collaboration with Other Countries: India collaborates with international organizations and other countries to address the global issue of drug trafficking. This includes participation in efforts to combat the production and trafficking of illegal drugs.
-    It's important to note that drug culture and the challenges associated with it can vary widely across different regions of India, and the situation may evolve over time. Additionally, policies and attitudes toward drug use may change, and new initiatives may be introduced to address emerging issues.`,
+    description: ``,
     imgSrc: "https://images.app.goo.gl/SMaPFpRCFwFwpdTr7",
     href: "https://www.indiatoday.in/diu/story/all-you-need-to-know-about-drugs-substance-abuse-in-india-1860644-2021-10-04",
   },
