11dda2ed00def317bc75cb19b09982f771938369
vforvishal12 <52970035+vforvishal12@users.noreply.github.com>
2023-12-24 11:09:51 +0000

    3

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,67 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'OUR DRUG CULTURE',
+    description: `India, like many other countries, has a complex relationship with drugs, encompassing both traditional and contemporary aspects. Here are some key points related to drug culture in India:
+
+    Traditional Drug Use:
+    
+    Ayurveda and Traditional Medicine: India has a rich history of traditional medicine, including Ayurveda, which utilizes various herbs and plants for medicinal purposes. Some of these substances may have psychoactive properties, and their use is often deeply ingrained in cultural and religious practices.
+    Cannabis Use:
+    
+    Cultural and Religious Significance: Cannabis has been used in religious rituals in India for centuries. It is associated with Lord Shiva and is commonly consumed during festivals like Holi and Mahashivratri.
+    Legal Status: The legal status of cannabis is a topic of debate in India. While the recreational use of cannabis is largely illegal, there are some states where the use of marijuana for medicinal and religious purposes is permitted.
+    Opium and Other Narcotics:
+    
+    Opium Cultivation: India has a history of opium cultivation, and it remains one of the largest legal producers of opium for pharmaceutical purposes. However, illegal cultivation and trafficking of opium and its derivatives also pose significant challenges.
+    Illicit Drug Trade:
+    
+    Narcotics Trafficking: India faces challenges related to the trafficking of illicit drugs, including heroin, cocaine, and synthetic drugs. The country is both a source and a transit point for the illegal drug trade.
+    Government Initiatives: The government has implemented various measures to combat drug trafficking, including the enforcement of the Narcotic Drugs and Psychotropic Substances Act.
+    Youth and Substance Abuse:
+    
+    Increasing Concerns: There are growing concerns about substance abuse, especially among the youth, in urban areas. Commonly abused substances include alcohol, tobacco, prescription drugs, and various synthetic drugs.
+    Government Response:
+    
+    Legislation and Enforcement: The Indian government has enacted legislation to regulate and control the use of narcotics and psychotropic substances. However, the enforcement of these laws faces challenges due to factors such as corruption, poverty, and limited resources.
+    Public Awareness and Rehabilitation:
+    
+    Awareness Programs: Various organizations and government agencies work on awareness programs to educate the public about the dangers of drug abuse.
+    Rehabilitation Centers: There are rehabilitation centers across the country that provide support and treatment for individuals struggling with substance abuse.
+    International Cooperation:
+    
+    Collaboration with Other Countries: India collaborates with international organizations and other countries to address the global issue of drug trafficking. This includes participation in efforts to combat the production and trafficking of illegal drugs.
+    It's important to note that drug culture and the challenges associated with it can vary widely across different regions of India, and the situation may evolve over time. Additionally, policies and attitudes toward drug use may change, and new initiatives may be introduced to address emerging issues.`,
+    imgSrc: 'https://images.app.goo.gl/SMaPFpRCFwFwpdTr7',
+    href: 'https://www.indiatoday.in/diu/story/all-you-need-to-know-about-drugs-substance-abuse-in-india-1860644-2021-10-04',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'WHAT IS MARIJUANA?',
+    description: `Marijuana is one of the most abused drugs in the world. There is an ever-growing gap between the latest science about marijuana and the myths surrounding it. Some people think that since it is legal in some places, it must be safe. But your body doesn’t know a legal drug from an illegal drug. It only knows the effect the drug creates once you have taken it. The purpose of this publication is to clear up some of the misunderstandings about pot.
+
+    Marijuana comes from the Indian hemp plant, and the part that contains the “drug” is found primarily in the flowers (commonly called the “buds”) and much less in the seeds, leaves, and stems of the plant.
+    
+    Marijuana is one of the most abused drugs in the world.
+    Marijuana, when sold, is a mixture of dried out leaves, stems, flowers and seeds of the hemp plant. It is usually green, brown or gray in color.
+    
+    Hashish is tan, brown or black resin that is dried and pressed into bars, sticks or balls. When smoked, both marijuana and hashish give off a distinctive, sweet odor.
+    
+    There are over 400 chemicals in marijuana and hashish.1 The chemical that causes intoxication or the “high” in users is called THC (short for tetrahydrocannabinol). THC creates the mind-altering effects that classifies marijuana as a “drug.”
+    
+    Plants, like animals, have traits that protect them in the wild. Plants can have colors or patterns that camouflage them from predators, or they can contain poisons or toxins that, when eaten, make animals sick or alter their mental capacity, putting them at risk in the wild. THC is the protective mechanism of the marijuana plant.
+    
+    Intoxication literally means “to poison by taking a toxic substance into your body.” Any substance that intoxicates causes changes in the body and the mind. It can create addiction or dependence, causing a person to want to take that drug even if it harms him or her.
+    
+    You may have heard someone say that because marijuana is a plant, it’s “natural” and so it’s harmless. But it’s not. Hemlock, a poisonous plant, is also “natural,” but it can kill.
+    
+    The other thing to know is that burning dried leaves and buds and inhaling the smoke into your lungs is definitely not “natural” and like smoking cigarettes, can be harmful to your body.
+    
+    As for the medical uses of marijuana, it contains another chemical called CBD (short for cannabidiol). This is the substance most often associated with creating medical benefits. Unlike THC, CBD does not cause a high.2 Its medical benefits are still being studied, as are methods to breed marijuana plants with high CBD and low THC for medical use.
+    
+    Marijuana is a drug like alcohol, cocaine, or ecstasy. And like these other drugs, it has side effects that can be harmful.
+    
+    `,
+    imgSrc: 'https://en.wikipedia.org/wiki/File:Cannabis_leaf.svg',
+    href: 'https://en.wikipedia.org/wiki/Cannabis_in_India',
   },
 ]
 
